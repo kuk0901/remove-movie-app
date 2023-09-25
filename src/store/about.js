837234f36a43ede9b450399878dfc24eb712168d
kuk0901 <kukhahyeon@gmail.com>
2023-09-25 17:15:20 +0900

    Create project

---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+import { Store } from "../core/heropy";
+
+export default new Store({
+  photo: "",
+  name: "DOBBY / KukHaHyeon",
+  email: "kukhahyeon@gmail.com",
+  blog: "None",
+  github: "https://github.com/kuk0901",
+  repository: "https://github.com/ParkYoungwoong/vanillajs-movie-app"
+});
